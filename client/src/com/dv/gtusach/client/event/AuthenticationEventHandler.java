2b8c1d537ec6d775bc3de9674ec13d22a74fbd18
one7piece <one7piece@gmail.com>
2015-04-16 14:30:03 +1000

    updated

--- client/src/com/dv/gtusach/client/event/AuthenticationEventHandler.java
+++
@@ -1,7 +0,0 @@
-package com.dv.gtusach.client.event;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface AuthenticationEventHandler extends EventHandler {
-	void onAuthenticationChanged(AuthenticationEvent event);
-}
--- client/src/com/dv/gtusach/client/event/PropertyChangeEventHandler.java
+++
@@ -1,7 +0,0 @@
-package com.dv.gtusach.client.event;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface PropertyChangeEventHandler extends EventHandler {
-	void onPropertyChanged(PropertyChangeEvent event);
-}
